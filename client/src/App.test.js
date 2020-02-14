7e1c432dfc6e62eded0558728bdfe3254a87ccfc
Alison Law <alisparklaw@gmail.com>
2020-02-14 09:53:57 +0000

    basic test set up

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as rtl from "@testing-library/react";
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders text "Womens World Cup- Player ranking', () => {
+  const wrapper = rtl.render(<App />);
+  const headerText = wrapper.queryByText(/Womens World Cup- Player ranking/i);
+  expect(headerText).toBeTruthy();
+  // expect(headerText).toMatch("Womens World Cup- Player ranking");
 });
