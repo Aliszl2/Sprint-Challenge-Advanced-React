3dd2b758c1ce9e54a7c41d67a265f2a5cf5cac76
Alison Law <alisparklaw@gmail.com>
2020-02-14 08:37:30 +0000

    dark mode functionality with hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import WorldcupData from "./components/Worldcupdata";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 import './App.css';
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-    {/* <Navbar /> */}
+    <Navbar />
     <h1>Womens World Cup- Player ranking</h1>
     <WorldcupData />
   </div>
